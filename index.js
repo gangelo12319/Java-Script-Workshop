cc5fb3230bb3effa6c49be8401b2f430b544e826
PoldePol <127740822+PoldePol@users.noreply.github.com>
2025-02-20 17:03:48 +0800

    Pokedex OJT

---
+++ index.js
@@ -0,0 +1,26 @@
+// < !DOCTYPE html >
+//     <html>
+//         <body>
+
+//             <h1>JavaScript Objects</h1>
+//             <h2>Object Destructuring</h2>
+
+//             <p id="demo"></p>
+
+//             <script>
+//                 var name = "invisible";
+
+//   // Define the function
+//   const person = () => {
+//                     let fname = "Paul";
+//                 let lname = "Alan";
+//                 // Return the full name from the function
+//                 return fname + " " + lname;
+//   }
+
+//                 // Display the result by calling the function
+//                 document.getElementById("demo").innerHTML = person();
+//             </script>
+
+//         </body>
+//     </html>
